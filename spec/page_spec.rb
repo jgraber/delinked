9e960d2842029a2df121d3436e8aa027be529bcb
Johnny Graber <jg@jgraber.ch>
2014-01-19 17:45:10 +0100

    be more specific what Page should do

--- spec/page_spec.rb
+++ spec/page_spec.rb
@@ -4,5 +4,12 @@ describe "A Page" do
   it "can store the tile and the URL" do
     page = Page.new("http://github.com", "GitHub")
     "GitHub".should eq(page.title)
+    "http://github.com".should eq(page.url)
+  end
+
+  it "can store a status info" do
+    page = Page.new("","")
+    page.status = 303
+    303.should eq(page.status)
   end
 end
